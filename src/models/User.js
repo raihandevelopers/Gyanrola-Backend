dd78cea459ba7572e47e5e4fed8629ba3319acfb
Antriksh Narang <antrikshnarang6@gmail.com>
2025-03-09 03:44:17 +0530

    Added Backend Wallet default to 0

--- src/models/User.js
+++ src/models/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
   role: { type: String, enum: ["user", "admin"], default: "user" },
   points: { type: Number, default: 0 }, // Add points field
   referredBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Add referredBy field
+  wallet: { type: Number, default: 0 }, // Add wallet field
 });
 
 module.exports = mongoose.model("User", userSchema);
