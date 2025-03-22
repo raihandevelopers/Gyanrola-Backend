2e2b4d00df50a1bb73022fd7e288675b9d661cde
raihandevelopers <raihankhan.indore@gmail.com>
2025-03-22 17:10:25 +0530

    changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token) return res.status(401).json({ error: "Access denied" });
-
+// d
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
