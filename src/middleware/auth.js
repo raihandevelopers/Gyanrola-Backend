19376f637f9b97bd13fdd1c4a229becbc257cde8
raihandevelopers <raihankhan.indore@gmail.com>
2025-04-16 20:17:04 +0530

    test

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,6 +5,7 @@ const authMiddleware = (req, res, next) => {
   if (!token) return res.status(401).json({ error: "Access denied" });
 // d
 // d
+// dnew
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
