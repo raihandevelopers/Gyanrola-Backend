5ebe416b0d9f37a9f1d50ef3bae40ddeb09b9e11
raihandevelopers <raihankhan.indore@gmail.com>
2025-07-10 20:28:45 +0530

    new

--- src/server.js
+++ src/server.js
@@ -19,6 +19,8 @@ const allowedOrigins = [
   "https://quiz-admin-fe-nj3j.vercel.app",
   "https://quiz-user-frontend.vercel.app",
   "https://playquizcontest.com",
+  "https://gyaaneralo.com",
+  "https://gyanrola.vercel.app",
   "http://localhost:5173", // For Local Admin Development
 ];
 
